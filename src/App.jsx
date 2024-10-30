061f0a50a73e8c1450a0a3edb144a5f6bbf7775d
Shubham Thokare <shubhamthokare1999@gmail.com>
2024-10-30 11:12:48 +0530

    amplify config

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -6,28 +7,36 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1)
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <header>
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+      </header>
+      <main className="card">
+        <button onClick={handleIncrement}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </main>
+      <footer>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </footer>
     </>
   )
 }
